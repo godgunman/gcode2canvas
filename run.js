cc9d5ff4da7c2feeb5d0e791adb5af050be8757b
godgunman <godgunman@gmail.com>
2015-10-19 00:07:10 +0800

    use asnyc to parallelize drawing

--- run.js
+++ run.js
@@ -5,8 +5,7 @@
             var text, line;
             text = $('#gcodeText').val();
             line = parse(text);
-            draw(line);
-            console.log(line);
+            drawAll(line);
         });
     });
 }());
@@ -22,46 +21,78 @@ function parse(data) {
         if (indexOfComment !== -1) {
             d = d.slice(0, d.indexOf(';'));
         }
-        if (d.indexOf('X') === -1 || d.indexOf('Y') === -1) {
-            return;
-        }
         
         part = d.split(' ');
-        if (part[0] === 'G1') {
+        if (part[0] === 'G1' && d.indexOf('X') !== -1 && d.indexOf('Y') !== -1) {
             x = parseFloat(part[1].slice(1));
             y = parseFloat(part[2].slice(1));
             line.push([x, y]);
 
-        } else if (part[0] === 'G0') {
+        } else if (part[0] === 'G0' && d.indexOf('X') !== -1 && d.indexOf('Y') !== -1) {
             x = parseFloat(part[1].slice(1));
             y = parseFloat(part[2].slice(1));
             line.push([x, y]);
+        } else if (part[0] === 'M03' || part[0] === 'M05'){
+            line.push(part);            
         }
     });
     return line;
 }
 
-function draw(line) {
+function genDrawLine(ctx, x1, y1, x2, y2) {
+
+    return function () {
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+        console.log('line done.');
+    };
+}
+
+function runTasks(tasks) {
+    
+    var TASK_LIMIT = 1000;
+    var INTERVAL_TIME = 500;
+    var taskStart = 0;
+    
+    setInterval(function() {
+        async.parallel(tasks.slice(taskStart, taskStart + TASK_LIMIT), function() {
+            console.log('done');        
+        });
+        taskStart += TASK_LIMIT;
+    }, INTERVAL_TIME);    
+    
+}
+
+function drawAll(line) {
     'use strict';
     
     var OFFSET = 30;
     var SCALE = 10;
 
     var ctx = $('#myCanvas')[0].getContext('2d'), i;
-    for (i = 0; i < line.length; i += 2) {
-        var x1, y1, x2, y2;
-        x1 = (line[i][0] + OFFSET) * SCALE;
-        y1 = (line[i][1] + OFFSET) * SCALE;
-
-        x2 = (line[i+1][0] + OFFSET) * SCALE;
-        y2 = (line[i+1][1] + OFFSET) * SCALE;
+    
+    var tasks = [], laserOn = false;
+    var x = -1, y = -1, newX, newY;
+    
+    for (i = 0; i < line.length; i ++) {        
         
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-        
-        console.log(x1, y1, x2, y2);
+        if (line[i][0] === 'M03') {
+            laserOn = true;
+        } else if (line[i][0] === 'M05') {
+            laserOn = false;
+        } else {        
+            newX = (line[i][0] + OFFSET) * SCALE;
+            newY = (line[i][1] + OFFSET) * SCALE;
+            if (laserOn === true && x !== -1 && y !== -1) {
+                tasks.push(genDrawLine(ctx, x, y, newX, newY));
+            }
+            x = newX;
+            y = newY;
+        }
     }
+    
+    runTasks(tasks);
 }
 
 
